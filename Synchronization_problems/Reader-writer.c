d49d4a6a158c650dd87e67a379356b7e5c2f0c5a
Siddharth Singh <108343500+Siddharth-Singh03@users.noreply.github.com>
2023-03-06 19:50:12 +0530

    Add files via upload

---
+++ Synchronization_problems/Reader-writer.c
@@ -0,0 +1,46 @@
+READER:-
+
+do {
+    
+   // Reader wants to enter the critical section
+   wait(mutex);
+
+   // The number of readers has now increased by 1
+   readcnt++;                          
+
+   // there is atleast one reader in the critical section
+   // this ensure no writer can enter if there is even one reader
+   // thus we give preference to readers here
+   if (readcnt==1)     
+      wait(wrt);                    
+
+   // other readers can enter while this current reader is inside 
+   // the critical section
+   signal(mutex);                   
+
+   // current reader performs reading here
+   wait(mutex);   // a reader wants to leave
+
+   readcnt--;
+
+   // that is, no reader is left in the critical section,
+   if (readcnt == 0) 
+       signal(wrt);         // writers can enter
+
+   signal(mutex); // reader leaves
+
+} while(true);
+
+
+WRITER:-
+
+do {
+    // writer requests for critical section
+    wait(wrt);  
+   
+    // performs the write
+
+    // leaves the critical section
+    signal(wrt);
+
+} while(true);
